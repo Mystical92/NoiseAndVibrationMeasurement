accac1a543a8c81c485b2aac17d8358cd57ba344
Kamil <kamil.sznejder.92@gmail.com>
2017-04-10 20:10:13 +0200

    project files "update", added important AVR/C libs to main.c, added USART functionality test in main()

--- NoiseAndVibrationMeasurement/main.c
+++ NoiseAndVibrationMeasurement/main.c
@@ -6,6 +6,10 @@
  */ 
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #include "src/mylibraries/usart/usart.h"
 #include "src/mylibraries/peripherals/peripherals.h"
@@ -17,8 +21,19 @@ int main(void)
 	enableOscillator_32mhz();
 	USART_configurate();
 
-    while (1) 
-    {
-    }
+	USART_send(&USARTC0, "Start\n");
+
+	uint16_t counter = 0;
+	while (1)
+	{
+		char str[15];
+		sprintf(str, "%d", counter);
+		counter++;
+		USART_send(&USARTC0, str);
+		USART_send(&USARTC0, ". ");
+		USART_send(&USARTC0, "while Loop Start\n");
+		_delay_ms(2000);
+		USART_send(&USARTC0, "while Loop End\n");
+	}
 }
 
